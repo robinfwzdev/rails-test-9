52e6b29d05b9efd12d88aa406037ce900361359b
robinfwzdev <robin@futureworkz.com>
2016-05-12 23:55:40 +0700

    Create data seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Shoe.destroy_all
+
+Shoe.create!(sku: "MX608V4", name: "New Balance Men's MX608V4 Training Shoe", price: "74.99")
+Shoe.create!(sku: "MX608V4", name: "Nike Free 4.0 Flyknit Sz 13 Mens Running Shoes", price: "125.00")
+Shoe.create!(sku: "MX608V4", name: "Adidas Performance Men's Galactic Elite M Running Shoe", price: "62.99")
+Shoe.create!(sku: "MX608V4", name: "Nike Free 5.0 Tr Fit 5 Synthetic Running Shoe", price: "103.99")
+Shoe.create!(sku: "MX608V4", name: "PUMA Men's Tazon 6 Cross-Training Shoe", price: "99.99")
+Shoe.create!(sku: "MX608V4", name: "Nike Free 5.0 Tr Fit 5 Print Sz 12 Womens Cross Training Shoes", price: "74.99")
