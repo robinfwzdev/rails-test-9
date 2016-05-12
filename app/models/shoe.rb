be171faf2c566f21cfa9cd95bf7cdef1e1b6516b
robinfwzdev <robin@futureworkz.com>
2016-05-12 22:18:24 +0700

    Validate price for shoe

--- app/models/shoe.rb
+++ app/models/shoe.rb
@@ -1,3 +1,4 @@
 class Shoe < ActiveRecord::Base
-  validates :name,              presence: true
+  validates :name, presence: true
+  validates :price, numericality: { greater_than: 0, allow_blank: true }
 end
--- spec/models/shoe_spec.rb
+++ spec/models/shoe_spec.rb
@@ -3,5 +3,6 @@ require 'rails_helper'
 RSpec.describe Shoe, type: :model do
   context 'validations' do
     it { is_expected.to validate_presence_of :name }
+    it { is_expected.to validate_numericality_of(:price).is_greater_than(0) }
   end  
 end
