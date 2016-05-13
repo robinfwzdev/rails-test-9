43ce3bdf35bed29af4b97261cbf2e5d3e60e38d8
robinfwzdev <robin@futureworkz.com>
2016-05-13 21:33:33 +0700

    Add belongs_to brand for shoe

--- app/models/shoe.rb
+++ app/models/shoe.rb
@@ -1,4 +1,6 @@
 class Shoe < ActiveRecord::Base
+  belongs_to :brand
+  
   validates :name, presence: true
   validates :price, numericality: { greater_than: 0, allow_blank: true }
 end
--- spec/models/shoe_spec.rb
+++ spec/models/shoe_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Shoe, type: :model do
+  context 'associations' do
+    it { is_expected.to belong_to :brand }
+  end
+
   context 'validations' do
     it { is_expected.to validate_presence_of :name }
     it { is_expected.to validate_numericality_of(:price).is_greater_than(0) }
