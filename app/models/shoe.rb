7e846658cd9b3bf16a7aa0ccd2a926f37ef75708
robinfwzdev <robin@futureworkz.com>
2016-05-13 22:02:36 +0700

    Change path defautl picture for shoe

--- app/models/shoe.rb
+++ app/models/shoe.rb
@@ -7,7 +7,7 @@ class Shoe < ActiveRecord::Base
 
   has_attached_file :picture,
     styles: { thumb: '300x300#>' },
-    default_url: "photo/missing.png"
+    default_url: "shoes/missing.png"
   validates_attachment :picture,
     content_type: {content_type: /\Aimage\/.*\Z/},
     size: { in: 0..5.megabytes, message: 'Photo size too large. Please limit to 5 mbs.' }
