6e1f849d5251dbd3ba37b9ab3b733cf510009750
robinfwzdev <robin@futureworkz.com>
2016-05-13 21:14:13 +0700

    Create brands table

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160512150520) do
+ActiveRecord::Schema.define(version: 20160513141212) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "brands", force: :cascade do |t|
+    t.string "name"
+  end
 
   create_table "photos", force: :cascade do |t|
     t.string   "picture_file_name"
