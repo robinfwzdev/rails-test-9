22b3581904027141cd7cb6a094fb40f690423809
robinfwzdev <robin@futureworkz.com>
2016-05-13 21:36:20 +0700

    Validate sku in shoe

--- app/models/shoe.rb
+++ app/models/shoe.rb
@@ -1,6 +1,7 @@
 class Shoe < ActiveRecord::Base
   belongs_to :brand
-  
+
   validates :name, presence: true
+  validates :sku, presence: true
   validates :price, numericality: { greater_than: 0, allow_blank: true }
 end
--- spec/models/shoe_spec.rb
+++ spec/models/shoe_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe Shoe, type: :model do
 
   context 'validations' do
     it { is_expected.to validate_presence_of :name }
+    it { is_expected.to validate_presence_of :sku }
     it { is_expected.to validate_numericality_of(:price).is_greater_than(0) }
   end
 end
