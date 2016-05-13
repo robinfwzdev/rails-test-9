e339b7c979e71ab464ab7f9741e973fd36998400
robinfwzdev <robin@futureworkz.com>
2016-05-13 21:22:12 +0700

    Remove association photo out of products

--- app/models/shoe.rb
+++ app/models/shoe.rb
@@ -1,6 +1,4 @@
 class Shoe < ActiveRecord::Base
-  has_many :photos
-  
   validates :name, presence: true
   validates :price, numericality: { greater_than: 0, allow_blank: true }
 end
--- spec/models/shoe_spec.rb
+++ spec/models/shoe_spec.rb
@@ -5,8 +5,4 @@ RSpec.describe Shoe, type: :model do
     it { is_expected.to validate_presence_of :name }
     it { is_expected.to validate_numericality_of(:price).is_greater_than(0) }
   end
-
-  context 'associations' do
-    it { is_expected.to have_many :photos }
-  end  
 end
