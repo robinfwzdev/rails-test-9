c5384b92765ec467f8f39922876de7ee791e2795
robinfwzdev <robin@futureworkz.com>
2016-05-13 21:42:00 +0700

    Validate picture field on shoe

--- app/models/shoe.rb
+++ app/models/shoe.rb
@@ -4,4 +4,11 @@ class Shoe < ActiveRecord::Base
   validates :name, presence: true
   validates :sku, presence: true
   validates :price, numericality: { greater_than: 0, allow_blank: true }
+
+  has_attached_file :picture,
+    styles: { thumb: '300x300#>' },
+    default_url: "photo/missing.png"
+  validates_attachment :picture,
+    content_type: {content_type: /\Aimage\/.*\Z/},
+    size: { in: 0..5.megabytes, message: 'Photo size too large. Please limit to 5 mbs.' }
 end
--- spec/models/shoe_spec.rb
+++ spec/models/shoe_spec.rb
@@ -9,5 +9,14 @@ RSpec.describe Shoe, type: :model do
     it { is_expected.to validate_presence_of :name }
     it { is_expected.to validate_presence_of :sku }
     it { is_expected.to validate_numericality_of(:price).is_greater_than(0) }
+    it do
+      is_expected.to validate_attachment_content_type(:picture)
+        .allowing('image/png', 'image/gif')
+        .rejecting('text/plain', 'text/xml')
+    end
+    it do
+      is_expected.to validate_attachment_size(:picture)
+        .less_than(5.megabytes)
+    end
   end
 end
