1ff2118103fbd69a96f35c6ca4b5070027b83c64
robinfwzdev <robin@futureworkz.com>
2016-05-14 14:15:18 +0700

    Create find by brands and price for shoe

--- app/models/shoe.rb
+++ app/models/shoe.rb
@@ -11,4 +11,16 @@ class Shoe < ActiveRecord::Base
   validates_attachment :picture,
     content_type: {content_type: /\Aimage\/.*\Z/},
     size: { in: 0..5.megabytes, message: 'Photo size too large. Please limit to 5 mbs.' }
+
+  def self.find_by_brands(brand_ids)
+    where(brand: brand_ids)
+  end
+
+  def self.find_by_min_price(min_price)
+    where("price >= ?", min_price)
+  end
+
+  def self.find_by_max_price(max_price)
+    where("price <= ?", max_price)
+  end  
 end
